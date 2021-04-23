aac994d48f70f10cd660973309683697745110d9
Raffy Doquenia <Raffy10288@yahoo.com.ph>
2021-04-23 22:00:07 +0800

    Add cpc help header (help.h)
    To provide simple help on using 'cpc' command.

---
+++ src/help.h
@@ -0,0 +1,37 @@
+/* C Project Creator - A command line tool that setup basic C project.
+ * Copyright (C) 2021 Radoq10288
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+
+/* File:			help.h
+ *
+ * Author:			Radoq10288
+ *
+ * Description:		Function to show help for C project creator.
+ *
+ * Date created:	04-17-2021-11:16:32-AM
+ */
+
+
+#ifndef _HELP_H_
+#define _HELP_H_
+
+	int cpc_help(char *help_args);
+
+#endif // _HELP_H_INCLUDED_
+
+
