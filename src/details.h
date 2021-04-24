39214aa261746c0e01cfb400f42f098e2c19133b
Raffy Doquenia <Raffy10288@yahoo.com.ph>
2021-04-24 14:58:37 +0800

    Add details header file (details.h)
    For showing details about the program and its license.

---
+++ src/details.h
@@ -0,0 +1,21 @@
+/* File:			details.h
+ *
+ * Author:			Radoq10288
+ *
+ * Description:		Shows details about C project creator, warranty
+ *					and conditions part of license.
+ *
+ * Date created:	04-17-2021-9:48:07-PM
+ */
+
+
+#ifndef _LAAD_H_
+#define _LAAD_H_
+
+	int cpc_about(void);
+	int cpc_conditions(void);
+	int cpc_warranty(void);
+
+#endif // _LAAD_H_INCLUDED_
+
+
