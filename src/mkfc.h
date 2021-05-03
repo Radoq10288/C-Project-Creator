8a3c9abe6376c872da016dc4462f8571bcb0446d
Raffy Doquenia <Raffy10288@yahoo.com.ph>
2021-05-03 12:58:56 +0800

    Add create_gsl_makefile()
    To give C project creator the ability to build GCC library. Not just
    to build executable file.

--- src/mkfc.h
+++ src/mkfc.h
@@ -46,6 +46,21 @@
 	 */
 	int create_exec_makefile(char *c_filename);
 
+
+	/* Functions that create makefile to build GCC library.
+	 */
+
+	/* Description:	Creates makefile to build GCC static library.
+	 *
+	 * Parameter:
+	 *   c_file	-	filename of the created C file.
+	 *   h_file -	filename of the created C header file.
+	 *
+	 * Return:		If function succeed, return 0. Else, return non-
+	 *				negative number.
+	 */
+	int create_gsl_makefile(char *c_file, char *h_file);
+
 #endif // _MKFC_H_INCLUDED_
 
 
